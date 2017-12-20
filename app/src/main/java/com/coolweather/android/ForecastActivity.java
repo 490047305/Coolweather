f726f1a5597eea76bb67fd6aa0b9cdabf79d8fa8
zengxiuyu <zengxiuyu321@163.com>
2017-12-20 17:35:30 +0800

    第二个版本，对主布局进行了修改

---
+++ app/src/main/java/com/coolweather/android/ForecastActivity.java
@@ -0,0 +1,13 @@
+package com.coolweather.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ForcastActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_forcast);
+    }
+}
